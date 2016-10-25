867797d6601f67ab085eebdb1c723d46e50c9712
madrid95 <madriddajsinani@gmail.com>
2016-10-25 11:13:44 +0200

    restore

--- src/VM/Assembler.java
+++ src/VM/Assembler.java
@@ -1,17 +1,32 @@
 package VM;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
 
 public class Assembler {
 	
-	int[] befehlsListe;
+	ArrayList<Integer> befehlsListe;
 	
+	/**Diese Funktion liefert den Befehl am gewünschten Index der Befehlsliste*/
 	public int getOrder(int index){
-		return befehlsListe[index];
+		return befehlsListe.get(index);
 	}
 	
-	public Assembler(String Filename){
-	
+	/**Dieser Konstruktor erstellt eine Befehlsliste aus einem Textdokument mit dem Filename des übergebenen Strings
+	 * Auf diese Befehlsliste kann dann mit int getOrder(int index) zugegriffen werden.
+	 * Falls Ausnahmen Auftreten, wird eine AssemblerException geworfen.*/
+	public Assembler(String Filename) throws AssemblerException{
+		try{
+			BufferedReader file= new BufferedReader(new FileReader(Filename));
+			String befehl=null;
+			
+			while((befehl=file.readLine())!=null){
+				befehlsListe.add(Integer.parseInt(befehl));
+			}
+					
+		}catch(Exception e){
+			throw new AssemblerException(e.getMessage());
+		}
 	}
 
 }
