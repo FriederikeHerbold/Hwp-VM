b1e6b46cc59aeade4ff05f02e3e35f91ef668ea7
FlorianKutz1522784 <florian.kutz@t-online.de>
2016-10-25 11:26:18 +0200

    Basic Changes on VM
    VM is now a subclass of Thread and has a constructor.

--- src/VM/VM.java
+++ src/VM/VM.java
@@ -1,10 +1,17 @@
 package VM;
 
-public class VM {
+public class VM extends Thread{
+	
+	Assembler myAssembler;
 
 	public static void main(String[] args) {
 	
 			
+	}
+	
+	/**This constructor creates a VM and its Assembler with the Code of the Text-File with the name Filename.*/
+	public VM(String Filename) throws AssemblerException{
+		myAssembler=new Assembler(Filename);
 	}
 
 }
