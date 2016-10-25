a734a158e6f64d1d6710b636757bc74188c87f34
Friederike Herbold <f.herbold@googlemail.com>
2016-10-25 11:24:17 +0200

    fragment
    fragment zur decodierung

--- src/VM/VM.java
+++ src/VM/VM.java
@@ -5,7 +5,7 @@ public class VM {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// kleiner test
-
+		
 	}
 
 }
---
+++ src/VM/decodierung.java
@@ -0,0 +1,22 @@
+package VM;
+
+public class decodierung {
+	int eingang = 2;
+	int befehl = eingang & 0b111;
+	int wert;
+	int rx;
+	int ry;
+	int from_mem;
+	int to_mem;
+	{
+		if (befehl == 0b0001 || befehl == 0b1001 || befehl == 0b1010 || befehl == 0b1011 || befehl == 0b1100) {
+			wert = (eingang << 4) & 0b111111111111;
+
+		} else {
+			rx = (eingang << 4) & 0b1111;
+			ry = (eingang << 8) & 0b1111;
+			from_mem = (eingang << 12) & 0b1;
+			to_mem = (eingang << 13) & 0b1;
+		}
+	}
+}
