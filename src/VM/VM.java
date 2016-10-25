ce50a14e495a73930b92605e76f210755e44e16a
FlorianKutz1522784 <florian.kutz@t-online.de>
2016-10-25 11:38:50 +0200

    Main
    I added a little Main method for testing

--- src/VM/VM.java
+++ src/VM/VM.java
@@ -1,12 +1,14 @@
 package VM;
 
+import static gdi.MakeItSimple.*;
+
 public class VM extends Thread{
 	
 	Assembler myAssembler;
 
-	public static void main(String[] args) {
-	
-			
+	public static void main(String[] args) throws AssemblerException {
+		VM myVM=new VM(readLine());
+		myVM.start();
 	}
 	
 	/**This constructor creates a VM and its Assembler with the Code of the Text-File with the name Filename.*/
