49f9ef837caca0977940dc6f8170db7ad7a61abf
FlorianKutz1522784 <florian.kutz@t-online.de>
2016-11-15 09:50:10 +0100

    Update 15.11.

---
+++ offtopic/task.js
@@ -0,0 +1,63 @@
+
+
+var task_data=function(data){
+
+  var code = data.map((val,index) => {
+
+    return '<tr><td>' + Object.values(val).join('</td><td>') + '</td></tr>';
+
+  }).join('\n');
+
+  return code;
+
+};
+
+var task = new XMLHttpRequest();
+
+task.open('GET', 'http://botnet.artificial.engineering:80/api/Tasks');
+
+task.responseType = 'json';
+
+task.setRequestHeader('Token', 'my-token-1337');
+
+task.onload = function() {
+
+  var data = task.response;
+
+  if (data !== null) {
+
+    console.log(data); // Parsed JSON object
+
+    var element = document.querySelector('#tasks tbody');
+
+    element.innerHTML = task_data(data);
+
+  }
+
+};
+
+task.send(null);
+
+
+/*
+var task = new XMLHttpRequest();
+
+task.open('POST', 'http://botnet.artificial.engineering:80/api/Tasks');
+
+task.responseType = 'json';
+
+task.setRequestHeader('Token', 'my-token-1337');
+
+task.onload = function() {
+
+  var data = task.response;
+
+  if (data !== null) {
+
+    console.log(data); // Parsed JSON object
+
+  }
+
+};
+
+task.send(null);*/
