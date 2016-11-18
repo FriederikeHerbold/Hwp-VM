393b30ad97c1b9001dd50da536b8dcda6fc0e2dd
Friederike Herbold <f.herbold@googlemail.com>
2016-11-18 09:38:11 +0100

    bin gestern nicht mehr dazu gekommen

---
+++ offtopic/menue.js
@@ -0,0 +1,13 @@
+var seitenWechsel = function(id) {
+
+    var element = document.querySelector('#' + id);
+    if (element !== null) {
+        element.className = 'active';
+    }
+    var others = [].slice.call(document.querySelectorAll('section'));
+    for (var i = 0; i < others.length; i++) {
+        if (others[i] !== element) {
+            others[i].className = '';
+        }
+    }
+};
--- offtopic/status.js
+++ offtopic/status.js
@@ -1,53 +1,54 @@
-/*data.forEach((element,i)=>{
-
-  var table = document.querySelector("table");
-  var body = table.createTBody();
-  var row = body.insertRow(0);
-  var cell = row.insertCell(0);
-  cell.innerHTML= element.workload;
-  var cell = row.insertCell(1);
-  cell.innerHTML= element.ip;
-  var cell = row.insertCell(2);
-  cell.innerHTML= element.id;
-
-
-  console.log(element.id);
-  console.log(element.ip);
-  console.log(element.task);
-  console.log(element.workload);
-});
-};*/
-
-var status_data=function(data){
-  var code = data.map((val,index) => {
-    var button;
-    if(val.workload === 0){
-      button = '<label class="switch">' +
-      '<input type="checkbox">' +
-      '<div class="slider round"></div>' +
-      '</label>'
-    }
-    else{
-      button = '<label class="switch">' +
-      '<input type="checkbox" checked>' +
-      '<div class="slider round"></div>' +
-      '</label>'
-    }
-    return '<tr><td>' + Object.values(val).join('</td><td>') +'</td><td>'+ button + '</td></tr>';
-  }).join('\n');
-  return code;
+var status_data = function(data) {
+    var code = data.map((val, index) => {
+        var button;
+        if (val.workload === 0) {
+            button = '<label class="switch">' +
+                '<input type="checkbox" onchange= "POSTstat(' + val.id + ', ' + val.workload + ')" >' +
+                '<div class="slider round"></div>' +
+                '</label>'
+        } else {
+            button = '<label class="switch">' +
+                '<input type="checkbox" onchange= "POSTstat(' + val.id + ', ' + val.workload + ')" checked>' +
+                '<div class="slider round"></div>' +
+                '</label>'
+        }
+        return '<tr><td>' + Object.values(val).join('</td><td>') + '</td><td>' + button + '</td></tr>';
+    }).join('\n');
+    return code;
 };
 
-var stat = new XMLHttpRequest();
-stat.open('GET', 'http://botnet.artificial.engineering:80/api/Status');
-stat.responseType = 'json';
-stat.setRequestHeader('Token', 'my-token-1337');
-stat.onload = function() {
-  var data = stat.response;
-  if (data !== null) {
-    console.log(data); // Parsed JSON object
-    var element = document.querySelector('#status tbody');
-    element.innerHTML = status_data(data);
-  }
-};
-stat.send(null);
+function getStatus() {
+    var stat = new XMLHttpRequest();
+    stat.open('GET', 'http://botnet.artificial.engineering:80/api/Status');
+    stat.responseType = 'json';
+    stat.setRequestHeader('Token', 'meins-1337');
+    stat.onload = function() {
+        var data = stat.response;
+        if (data !== null) {
+            console.log(data); // Parsed JSON object
+            var element = document.querySelector('#status tbody');
+            element.innerHTML = status_data(data);
+        }
+    };
+    stat.send(null);
+}
+getStatus();
+setInterval(getStatus, 10000);
+
+function POSTstat(ids, workload) {
+    var statPOST = new XMLHttpRequest();
+    statPOST.open('POST', 'http://botnet.artificial.engineering:80/api/Status', true);
+    statPOST.responseType = 'json';
+    statPOST.setRequestHeader('Content-type', 'application/json; charset=UTF-8');
+    statPOST.setRequestHeader('Token', 'meins-1337');
+    var datenS = {
+        id: ids,
+        status: null
+    };
+    if (workload === 0) {
+        datenS.status = true;
+    } else {
+        datenS.status = false;
+    }
+    statPOST.send(JSON.stringify(datenS));
+}
