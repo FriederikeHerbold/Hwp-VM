8a9b45230374af1d150aa611075ae85781c2b184
madrid95 <madriddajsinani@gmail.com>
2016-11-22 10:33:40 +0100

    Command

---
+++ src/VM/StartVM.java
@@ -0,0 +1,16 @@
+package VM;
+
+public class StartVM {
+
+	public static void main(String[] args) {
+	
+		try {
+			new Thread(new VM("asda")).start();
+		} catch (AssemblerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
--- src/VM/VM.java
+++ src/VM/VM.java
@@ -1,26 +1,122 @@
 package VM;
 
+import java.util.Stack;
 
+public class VM implements Runnable {
 
-import java.util.Scanner;
-
-
-
-public class VM extends Thread {
-
-	Assembler myAssembler;
+	private Assembler myAssembler;
+	private Stack<Integer> stack=new Stack<Integer>();
+	private int prgCNT,cmd,opCode;
+	private int [] reg=new int[4096];
 	
-
-	public static void main(String[] args) throws AssemblerException {
+	
+	public VM (String fileName) throws AssemblerException {
 		
-		Scanner sc=new Scanner(System.in);
-		VM myVM = new VM(sc.nextLine());
-		myVM.start();
+		myAssembler=new Assembler(fileName);
+
 	}
 
-	/**This constructor creates a VM and its Assembler with the Code of the Text-File with the name Filename.*/
-		public VM(String Filename) throws AssemblerException{
-			myAssembler=new Assembler(Filename);
-		}
 
+	@Override
+	public void run() {
+		do{
+			opCode=myAssembler.getOrder(prgCNT);
+			cmd= (opCode) & 0b1111;
+			
+			makeMenu(cmd);
+				
+		}while(prgCNT<myAssembler.getSize());
+		
+	}
+
+
+	private void makeMenu(int command) {
+		
+	
+		switch(command){
+			
+		case(Command.NOP):
+			break;
+		case(Command.LOAD):
+			reg[0]=getValue(opCode);
+			prgCNT++;
+			break;
+		case(Command.MOV):
+			if(getToMem(opCode)==1){
+				reg[getIndexRx(opCode)]=reg[getIndexRy(opCode)];
+			}else if(getFromMem(opCode)==1){
+				reg[getIndexRy(opCode)]=reg[getIndexRx(opCode)];
+			}
+			prgCNT++;
+			break;
+		case(Command.ADD):
+			reg[getIndexRx(opCode)]+= reg[getIndexRy(opCode)];
+			prgCNT++;
+			break;
+		case(Command.SUB):
+			reg[getIndexRx(opCode)]-=reg[getIndexRy(opCode)];
+			prgCNT++;
+		case(Command.MUL):
+			reg[getIndexRx(opCode)]*=reg[getIndexRy(opCode)];
+			prgCNT++;
+			break;
+		case (Command.DIV):
+			reg[getIndexRx(opCode)]/=reg[getIndexRy(opCode)];
+			prgCNT++;
+		case(Command.PUSH):
+			stack.push(reg[getIndexRx(opCode)]);
+			prgCNT++;
+			break;
+		case(Command.POP):
+			stack.pop();
+			break;
+		case(Command.JMP):
+			prgCNT=getValue(opCode);
+			break;
+		case(Command.JIZ):
+			if(reg[0]==0){
+				prgCNT=getValue(opCode);
+			}
+			break;
+		case(Command.JIH):
+			if(reg[0]>0){
+				prgCNT=getValue(opCode);
+			}
+			break;
+		case(Command.JSR):
+			
+			stack.push(prgCNT+1);
+			
+			prgCNT=getValue(opCode);
+			
+			break;
+		case(Command.RTS):
+			if(stack.isEmpty()){
+				System.exit(0);
+			}
+			prgCNT=stack.pop();
+			
+			break;
+		default:
+			break;
+			
+		}
+	}
+	
+	private int getValue(int opCode){
+		return (opCode>>4) & 0b1111;
+	}
+	
+	private int getToMem(int opCode){
+		return (opCode>>13) & 1;
+	}
+	private int getFromMem(int opCode){
+		return (opCode>>12) & 1;
+	}
+	private int getIndexRx(int opCode){
+		return (opCode>>4)& 0b1111;
+	}
+	private int getIndexRy(int opCode){
+		return (opCode>>8) & 0b1111;
+	}
 }
