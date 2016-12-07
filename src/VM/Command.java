11eb46abf197c714c8413ed9bcf0a30a098ff6d4
madrid95 <madriddajsinani@gmail.com>
2016-12-07 13:53:35 +0100

    Update Command.java

--- src/VM/Command.java
+++ src/VM/Command.java
@@ -4,7 +4,8 @@ package VM;
 public interface Command {
 
 	final static String [] arrayString={"NOP","LOAD","MOV","ADD","SUB","MUL","DIV","PUSH","POP","JMP","JIZ","JIH","JSR","RTS"};
-	  final static int   [] arrayInt={0000,0001,0010,0011,0100,0101,0110,0111,1000,1001,1010,1011,1100,1101};
+	  final static int   [] arrayStringCode={"0000","0001","0010","0011","0100","0101","0110","0111","1000","1001","1010","1011",
+						 "1100","1101"};
 	
 	
 }
